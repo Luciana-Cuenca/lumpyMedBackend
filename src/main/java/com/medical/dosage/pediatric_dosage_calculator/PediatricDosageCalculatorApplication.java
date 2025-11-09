148c392485532fce24b778c9d84e761eb821b1e4
Luciana-Cuenca <luciana.cuenca@campusucc.edu.co>
2025-11-08 23:14:22 -0500

    backend, resources

---
+++ src/main/java/com/medical/dosage/pediatric_dosage_calculator/PediatricDosageCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.medical.dosage.pediatric_dosage_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PediatricDosageCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PediatricDosageCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medical/dosage/pediatric_dosage_calculator/PediatricDosageCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.dosage.pediatric_dosage_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PediatricDosageCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
