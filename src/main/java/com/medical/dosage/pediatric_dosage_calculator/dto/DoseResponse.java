ef4d431592d6971f977c6bf6b4b234c87dbfbadf
Luciana-Cuenca <luciana.cuenca@campusucc.edu.co>
2025-11-09 20:52:20 -0500

    dto: DoseResponse correncion sin edad

--- src/main/java/com/medical/dosage/pediatric_dosage_calculator/dto/DoseResponse.java
+++ src/main/java/com/medical/dosage/pediatric_dosage_calculator/dto/DoseResponse.java
@@ -3,20 +3,17 @@ package com.medical.dosage.pediatric_dosage_calculator.dto;
 public class DoseResponse {
     private String medicine;
     private double weightKg;
-    private int ageMonths;         // Edad en meses
     private double mgPerDay;
     private int dosesPerDay;
     private double mgPerDose;
     private double mlPerDose;
-    private String alert;          // Alertas AIEPI
-    private String safeRange;      // Rango seguro en ml
+    private String alert;
+    private String safeRange;
 
-    // Constructor
-    public DoseResponse(String medicine, double weightKg, int ageMonths, double mgPerDay,int dosesPerDay, double mgPerDose, double mlPerDose,String alert, String safeRange) 
-    {
+    public DoseResponse(String medicine, double weightKg, double mgPerDay, int dosesPerDay,
+                        double mgPerDose, double mlPerDose, String alert, String safeRange) {
         this.medicine = medicine;
         this.weightKg = weightKg;
-        this.ageMonths = ageMonths;
         this.mgPerDay = mgPerDay;
         this.dosesPerDay = dosesPerDay;
         this.mgPerDose = mgPerDose;
@@ -28,7 +25,6 @@ public class DoseResponse {
     // Getters
     public String getMedicine() { return medicine; }
     public double getWeightKg() { return weightKg; }
-    public int getAgeMonths() { return ageMonths; }
     public double getMgPerDay() { return mgPerDay; }
     public int getDosesPerDay() { return dosesPerDay; }
     public double getMgPerDose() { return mgPerDose; }
